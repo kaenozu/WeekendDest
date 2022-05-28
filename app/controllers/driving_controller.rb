d0419e4033f2aa5ebba92f6a2095ba0cc3b54ef1
kaenozu <kaenozu@gmail.com>
2022-05-28 12:19:37 +0900

    最新コミット

--- app/controllers/driving_controller.rb
+++ app/controllers/driving_controller.rb
@@ -1,36 +1,43 @@
 class DrivingController < ApplicationController
   def index
-    p 'index start'
+    p "index start"
   end
 
   def list
-    p 'list start'
-    return if params[:src].nil?
+    p "list start"
+    p "params[:src]:" + params[:src]
+    if params[:src].present?
+      geoReq = Api::GoogleGeo::Request.new
+      geoRes = geoReq.request(params[:src])
 
-    geoReq = Api::GoogleGeo::Request.new
-    geoRes = geoReq.request(params[:src])
-
-    routeRequest = Api::GoogleRoute::Request.new
-    route = []
-    randomList = []
-    Tourist.all.each do |s|
-      distance = Tourist.distance(geoRes['result']['latitude'], geoRes['result']['longitude'], s['latitude'].to_f,
-                                  s['longitude'].to_f)
-      # distance = Tourist.distance(params[:src].split(",")[0].to_f, params[:src].split(",")[1].to_f, s["latitude"].to_f, s["longitude"].to_f)
-      if distance < 100
-        randomList.push({ src: params[:src], dest: s['latitude'] + ',' + s['longitude'], place_name: s['place_name'] })
+      routeRequest = Api::GoogleRoute::Request.new
+      route = []
+      randomList = []
+      @result = []
+      @srcdestMap = {}
+      Tourist.all.each do |s|
+        distance = Tourist.distance(geoRes["result"]["latitude"], geoRes["result"]["longitude"], s["latitude"].to_f,
+                                    s["longitude"].to_f)
+        # distance = Tourist.distance(params[:src].split(",")[0].to_f, params[:src].split(",")[1].to_f, s["latitude"].to_f, s["longitude"].to_f)
+        if distance < 100
+          randomList.push({ src: params[:src], dest: s["latitude"] + "," + s["longitude"], place_name: s["place_name"] })
+        end
       end
+      if !randomList.empty?
+        @srcdestMap = randomList.sample
+        route.push(@srcdestMap)
+
+        routeRes = routeRequest.request(route)
+
+        unless routeRes.nil?
+          # 返却した値はそのまま画面へ
+          @result = routeRes["result"]
+        end
+      end
+
+      render :list
+    else
+      redirect_to action: :index
     end
-    @srcdestMap = randomList.sample
-    route.push(@srcdestMap)
-
-    routeRes = routeRequest.request(route)
-
-    unless routeRes.nil?
-      # 返却した値はそのまま画面へ
-      @result = routeRes['result']
-    end
-
-    render :list
   end
 end
