6b8d3b9c96cb897b0e611886572b72c49704a2d0
kaenozu <kaenozu@gmail.com>
2022-05-28 15:25:52 +0900

    migrateのみ実行

---
+++ app/models/route_cash.rb
@@ -0,0 +1,2 @@
+class RouteCash < ApplicationRecord
+end
---
+++ test/models/route_cash_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class RouteCashTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
